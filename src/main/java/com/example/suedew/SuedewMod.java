f7973a2c7c3a4090a251e306a86b428cccf0a25f
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-05-28 18:10:18 -0500

    Update SuedewMod.java

--- src/main/java/com/example/suedew/SuedewMod.java
+++ src/main/java/com/example/suedew/SuedewMod.java
@@ -1,10 +1,9 @@
-package com.example;
+package com.example.suedew;
 
-import com.example.suedew.SudoCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
-public class ExampleMod implements ModInitializer {
+public class SuedewMod implements ModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
