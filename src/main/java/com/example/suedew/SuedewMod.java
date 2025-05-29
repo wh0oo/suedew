8213c103dabcfa79eb4cd5300f775c9115e5e845
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-05-29 17:15:08 -0500

    Update SuedewMod.java

--- src/main/java/com/example/suedew/SuedewMod.java
+++ src/main/java/com/example/suedew/SuedewMod.java
@@ -2,13 +2,20 @@ package com.example.suedew;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SuedewMod implements ModInitializer {
+    public static final String MOD_ID = "suedew";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-        System.out.println("[Suedew] Mod initialized.");
+        LOGGER.info("Initializing Suedew mod");
+        
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             SudoCommand2.register(dispatcher);
+            LOGGER.info("Registered /sudo command");
         });
     }
 }
