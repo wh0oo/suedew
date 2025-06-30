071a0d48eb73271b9abd41d5e8834cc2b2cf3e4d
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-06-29 20:22:16 -0500

    Update SudoCommand2.java

--- src/main/java/com/example/suedew/SudoCommand2.java
+++ src/main/java/com/example/suedew/SudoCommand2.java
@@ -4,8 +4,8 @@ import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.command.argument.MessageArgumentType;
 import net.minecraft.network.message.MessageType;
@@ -14,15 +14,15 @@ import net.minecraft.server.PlayerManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
 public class SudoCommand2 {
     private static final SimpleCommandExceptionType PLAYER_NOT_FOUND =
         new SimpleCommandExceptionType(Text.literal("Targeted player could not be found"));
-    private static final Logger LOGGER = LogManager.getLogger("Suedew");
+    private static final Logger LOGGER = LoggerFactory.getLogger("Suedew");
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> cmd = literal("sudo")
@@ -47,8 +47,7 @@ public class SudoCommand2 {
                             PlayerManager pm = server.getPlayerManager();
 
                             // Log to server console via Logger
-                            LOGGER.info("[sudo][chat] " + src.getName() + " as " +
-                                        targetName + ": \"" + raw + "\"");
+                            LOGGER.info("[sudo][chat] {} as {}: \"{}\"", src.getName(), targetName, raw);
 
                             // Broadcast signed chat as <target>
                             MessageArgumentType.getSignedMessage(ctx, "message", signedMsg -> {
@@ -84,8 +83,7 @@ public class SudoCommand2 {
                         String withSlash = "/" + cmdPart; // e.g., "/say Hello"
 
                         // Log to server console via Logger
-                        LOGGER.info("[sudo][cmd] " + src.getName() + " as " +
-                                    targetName + ": \"" + withSlash + "\"");
+                        LOGGER.info("[sudo][cmd] {} as {}: \"{}\"", src.getName(), targetName, withSlash);
 
                         return target.getCommandSource(); // run with target’s permissions
                     })
