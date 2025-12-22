813d34b12c8f69bf9943f75c9f153c16d82c3e54
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-12-22 16:12:35 -0600

    Refactor command registration and logging

--- src/main/java/com/example/suedew/SuedewMod.java
+++ src/main/java/com/example/suedew/SuedewMod.java
@@ -6,16 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SuedewMod implements ModInitializer {
+
     public static final String MOD_ID = "suedew";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing Suedew mod");
-        
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            SudoCommand2.register(dispatcher);
-            LOGGER.info("Registered /sudo command");
-        });
+        LOGGER.info("Initializing Suedew");
+
+        CommandRegistrationCallback.EVENT.register(
+            (dispatcher, registryAccess, environment) -> {
+                SudoCommand.register(dispatcher);
+            }
+        );
     }
 }
